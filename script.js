abac730991029e644005f5785921b18c617bd974
abdu djuraev <abdudj575@gmail.com>
2026-02-19 09:38:53 -0500

    payment-02: Add form validation for expiration date and CVV

--- script.js
+++ script.js
@@ -3,5 +3,29 @@ submitBtn.addEventListener("click", handleFormSubmission);
 
 function handleFormSubmission(event){
     event.preventDefault();
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+
+    if(new Date() > new Date(year, month - 1)){
+        window.alert("Your card has expired!");
+        return;
+    }
+
+    const cvv = document.getElementById("cvv").value;
+    if (!/^[0-9]{3,4}$/.test(cvv)){
+        window.alert("Invalid CVV. Must be 3 or 4 digits!");
+        return;
+    }
+
+    const cnumber = document.getElementById("cnumber").value;
+    if(!isValid(cnumber)){
+        window.alert("Invalid card number!");
+        return;
+    }
+
     window.alert("Thanks for the payment!");
+}
+
+function isValid(cnumber){
+    return true;
 }
